c1ce0f5c9a0cca0a305d382044d25ce9cca42744
zou wendi <542095059@qq.com>
2019-12-03 09:14:39 +0800

    nth

--- app.js
+++ app.js
@@ -16,28 +16,37 @@ const apiPrefix = process.env.apiPrefix || "/api";
 signale.debug(`代理地址: ${target}`);
 
 const handlerSchemas = [];
+let loading = true;
 
 const interactive = new signale.Signale({ interactive: true });
-interactive.await("获取源数据配置中");
 
-request.get(
-  `${generalPath}${apiPrefix}/general/api/rules`,
-  (error, response, body) => {
-    if (error) signale.error(error);
-    else {
-      const data = JSON.parse(body);
-      if (data.code === 200) {
-        for (const api of data.data) {
-          const handlerData = getHandlePath(api.path, api.rules);
-          handlerSchemas.push(handlerData);
-        }
-        interactive.success("加载源数据成功");
+function readApiConfig() {
+  interactive.await("获取源数据配置中");
+
+  request.get(
+    `${generalPath}${apiPrefix}/general/api/rules`,
+    (error, response, body) => {
+      if (error) {
+        interactive.error(error);
+        readApiConfig();
       } else {
-        signale.error(`加载源数据失败: ${data.msg}`);
+        const data = JSON.parse(body);
+        if (data.code === 200) {
+          for (const api of data.data) {
+            const handlerData = getHandlePath(api.path, api.rules);
+            handlerSchemas.push(handlerData);
+          }
+          interactive.success("加载源数据成功");
+          loading = false;
+        } else {
+          interactive.error(`加载源数据失败: ${data.msg}`);
+          readApiConfig();
+        }
       }
     }
-  }
-);
+  );
+}
+readApiConfig();
 
 const proxy = httpProxy.createProxyServer({ target }).listen(8001);
 
@@ -61,6 +70,17 @@ proxy.on("error", function(err, req, res) {
 // Listen for the `proxyRes` event on `proxy`.
 //
 proxy.on("proxyReq", async function(proxyRes, req, res) {
+  if (loading) {
+    signale.error(req.method, req.url, errMsg);
+    res.writeHead(400, {
+      "Content-Type": "text/plain"
+    });
+    res.end("服务器正在启动中");
+    proxyRes.destroy(checkErrorCode);
+    proxyRes.abort();
+    return;
+  }
+
   const errMsg = await checkData(req, handlerSchemas);
   if (errMsg) {
     signale.error(req.method, req.url, errMsg);
